7eae209cac417db2e0dbfc9865bbfcc344bd3b5c
p3ggy <tranvanchien24022003@gmail.com>
2025-05-20 22:40:32 +0700

    update source code

--- hncgparty/lib/core/config/env_deployment.dart
+++ hncgparty/lib/core/config/env_deployment.dart
@@ -1,3 +1,3 @@
 abstract class Env {
-  static const String baseUrl = 'https://470c-113-167-120-223.ngrok-free.app';
+  static const String baseUrl = 'https://fed3-113-167-120-223.ngrok-free.app';
 }
